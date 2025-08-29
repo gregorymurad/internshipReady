a6999fca1f626ae39e8f7b0d5fa10f611990cc50
Dev Singh <devsingh@Devs-MacBook-Air.local>
2025-08-29 15:28:28 -0400

    Fixing the map widget of the dashboard

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ with maps:
                              lat="latitude",
                              lon="longitude",
                              mapbox_style="open-street-map",
-                             zoom=17,
+                             zoom=20,
                              hover_data=df)
     st.plotly_chart(fig3)
 
@@ -69,4 +69,8 @@ with tables:
     st.dataframe(df)
     st.divider()
     st.subheader("Descriptive Statistics")
-    st.dataframe(df.describe())
+    st.dataframe(df.describe()
+
+)
+:wq
+
